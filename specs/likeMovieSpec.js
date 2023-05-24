d9a5b53a6acc024c582c4c93db71928ba1ce00a8
I Nyoman Arijaya Putra <63515581+ArijayaPutra@users.noreply.github.com>
2023-05-24 16:32:54 +0700

    Update likeMovieSpec.js

--- specs/likeMovieSpec.js
+++ specs/likeMovieSpec.js
@@ -2,46 +2,45 @@ import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurant-idb';
 import * as TestFactories from './helpers/testFactories';
 
 describe('Liking A Restaurant', () => {
-  const addLikeButtonContainer = () => {
-    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
-  };
-  beforeEach(() => {
-    addLikeButtonContainer();
-  });
-  it( 'should show the like button when the Restaurant has not been liked before', async () =>
-  {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 })
-  
-    expect( document.querySelector( '[aria-label="like this Restaurant"]' ) ).toBeTruthy();
-  });
-  it('should not show the unlike button when the Restaurant has not been liked before', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant( { id: 1 } )
-    
-    expect(document.querySelector('[aria-label="unlike this Restaurant"]')).toBeFalsy();
-  });
-  it('should be able to like the Restaurant', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant( { id: 1 } )
-    
-    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
-    const Restaurant = await FavoriteRestaurantIdb.getRestaurant(1);
-    expect(Restaurant).toEqual({ id: 1 });
-    FavoriteRestaurantIdb.deleteRestaurant(1);
-  });
-  it('should not add a Restaurant again when its already liked', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 })
-    
-    await FavoriteRestaurantIdb.putRestaurant( { id: 1 } );
-    
-    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+	const addLikeButtonContainer = () => {
+		document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+	};
+	beforeEach(() => {
+		addLikeButtonContainer();
+	});
+	it('should show the like button when the Restaurant has not been liked before', async () => {
+		await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
 
-    expect(await FavoriteRestaurantIdb.getAllRestaurant()).toEqual([{ id: 1 }]);
-    FavoriteRestaurantIdb.deleteRestaurant(1);
-  });
+		expect(document.querySelector('[aria-label="like this Restaurant"]')).toBeTruthy();
+	});
+	it('should not show the unlike button when the Restaurant has not been liked before', async () => {
+		await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
 
-  it('should not add a Restaurant when it has no id', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant( {} )
-    
-    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
-    expect(await FavoriteRestaurantIdb.getAllRestaurant()).toEqual([]);
-  });
-  });
+		expect(document.querySelector('[aria-label="unlike this Restaurant"]')).toBeFalsy();
+	});
+	it('should be able to like the Restaurant', async () => {
+		await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+
+		document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+		const Restaurant = await FavoriteRestaurantIdb.getRestaurant(1);
+		expect(Restaurant).toEqual({ id: 1 });
+		FavoriteRestaurantIdb.deleteRestaurant(1);
+	});
+	it('should not add a Restaurant again when its already liked', async () => {
+		await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+
+		await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+
+		document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+		expect(await FavoriteRestaurantIdb.getAllRestaurant()).toEqual([{ id: 1 }]);
+		FavoriteRestaurantIdb.deleteRestaurant(1);
+	});
+
+	it('should not add a Restaurant when it has no id', async () => {
+		await TestFactories.createLikeButtonPresenterWithRestaurant({});
+
+		document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+		expect(await FavoriteRestaurantIdb.getAllRestaurant()).toEqual([]);
+	});
+});
